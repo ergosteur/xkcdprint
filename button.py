52651f23dcc832e492e8668aead7958a396807e6
ergosteur <ergosteur@gmail.com>
2017-08-20 03:14:37 -0400

    ==

--- button.py
+++ button.py
@@ -8,6 +8,6 @@ script = "python ./xkcdprint.py"
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonPin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
-    if (GPIO.input(buttonPin)):
+    if (GPIO.input(buttonPin) == 0):
         os.system(script)
         time.sleep(1)
