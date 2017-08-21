82941c32b4206778f0294ae516ed23d6dcb9d219
ergosteur <ergosteur@gmail.com>
2017-08-20 22:31:49 -0400

    try to add argument functionality

--- xkcdprint.py
+++ xkcdprint.py
@@ -3,19 +3,43 @@ import escpos
 import escpos.printer
 import xkcd
 import textwrap
+import sys
+import getopt
 from random import randint
 from PIL import Image
 
+usageText = 'xkcdprint.py [-n <number of comics to print>] [-r] [-i <comicid>]\nDefaults to printing latest comic.\n'
 nComicsToPrint = 1
 nComicsPrinted = 0
 comicRandom = False
 maxPixelWidth = 512
+nStartComic = xkcd.getLatestComic().number
 
-p = escpos.printer.Usb(0x04b8,0x0e15)
-nLatestComic = xkcd.getLatestComic().number 
+try:
+    opts, args = getopt.getopt(sys.argv,"hn:ri:",["help","to-print=","random","comicid="])
+except getopt.GetoptError:
+    print(usageText)
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-h':
+        print(usageText)
+        sys.exit()
+    elif opt in ("-r", "--random"):
+        nStartComic = randint(1, xkcd.getLatestComicNum())
+    elif opt in ("-i", "--comicid"):
+        if 0 <= arg <= xkcd.getLatestComicNum():
+            nStartComic = arg
+        else:
+            sys.exit(1)
+    elif opt in ("-n", "--to-print"):
+        nComicsToPrint = arg
+
+# Initialize Epson thermal printer using usb-id (here using TM-T20II)
+p = escpos.printer.Usb(0x04b8, 0x0e15)
 
 while nComicsPrinted < nComicsToPrint:
-    comic = xkcd.getComic(nLatestComic - nComicsPrinted)
+    comic = xkcd.getComic(nStartComic - nComicsPrinted)
     comicImg = comic.download()
     comicAltText = textwrap.wrap(comic.getAsciiAltText(),40)
     img = Image.open(comicImg.encode('utf-8'))
